f852045aad4470e492387c92e6d30d08247db91f
Andrey Ferreira <andreyferreirahhvj5@gmail.com>
2025-09-20 12:22:20 -0300

    adiciona exbição do item

---
+++ trabalho1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    // Exibicao dos dados
+    printf("\n--- Item Cadastrado ---\n");
+    printf("ID: %d\n", item_id);
+    printf("Nome: %s\n", item_nome);
+    printf("Preco: R$ %.2f\n", item_preco);
+
+    return 0;
+}
