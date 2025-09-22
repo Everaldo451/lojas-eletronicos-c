319b8d6656c01065df72061457f246b649033a7b
Andrey Ferreira <andreyferreirahhvj5@gmail.com>
2025-09-22 10:41:01 -0300

    Update trabalho1.cpp

--- trabalho1.cpp
+++ trabalho1.cpp
@@ -1,13 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
+    // Dados do produto já definidos
+    char codigo_barras[] = "7891234567890";
+    char nome[] = "Arroz Tipo 1";
+    int dia = 10, mes = 9, ano = 2025;
+    float preco = 25.90;
+    char fabricante[] = "Alimentos Brasil LTDA";
+    int quantidade = 150;
+    int cadastrado = 1;
 
-    // Exibicao dos dados
-    printf("\n--- Item Cadastrado ---\n");
-    printf("ID: %d\n", item_id);
-    printf("Nome: %s\n", item_nome);
-    printf("Preco: R$ %.2f\n", item_preco);
+    if (cadastrado) {
+        printf("\n--- DADOS DO PRODUTO ---\n");
+        printf("Codigo de barras: %s\n", codigo_barras);
+        printf("Nome: %s\n", nome);
+        printf("Data de fabricacao: %02d/%02d/%04d\n", dia, mes, ano);
+        printf("Preco: R$ %.2f\n", preco);
+        printf("Fabricante: %s\n", fabricante);
+        printf("Quantidade em estoque: %d\n", quantidade);
+    } else {
+        printf("Vazio: Nenhum produto cadastrado.\n");
+    }
 
     return 0;
 }
+
